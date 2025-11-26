d4f3886265a967f7652826c9a1696ab5f4a1e383
NovaPixel <contact@novapixel.fr>
2025-11-26 17:01:04 +0100

    chore: add latest Payload migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20241125_222020_initial from './20241125_222020_initial';
 import * as migration_20241214_124128 from './20241214_124128';
 import * as migration_20251126_150345_add_bento_grid from './20251126_150345_add_bento_grid';
 import * as migration_20251126_151501 from './20251126_151501';
+import * as migration_20251126_155831_latest from './20251126_155831_latest';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20251126_151501.up,
     down: migration_20251126_151501.down,
-    name: '20251126_151501'
+    name: '20251126_151501',
+  },
+  {
+    up: migration_20251126_155831_latest.up,
+    down: migration_20251126_155831_latest.down,
+    name: '20251126_155831_latest'
   },
 ];
