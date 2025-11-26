34d628df9609c50b7f0dca07a9eb61f08e8d04bb
NovaPixel <contact@novapixel.fr>
2025-11-26 16:15:47 +0100

    chore: add Payload migration for schema updates

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241125_222020_initial from './20241125_222020_initial';
 import * as migration_20241214_124128 from './20241214_124128';
 import * as migration_20251126_150345_add_bento_grid from './20251126_150345_add_bento_grid';
+import * as migration_20251126_151501 from './20251126_151501';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20251126_150345_add_bento_grid.up,
     down: migration_20251126_150345_add_bento_grid.down,
-    name: '20251126_150345_add_bento_grid'
+    name: '20251126_150345_add_bento_grid',
+  },
+  {
+    up: migration_20251126_151501.up,
+    down: migration_20251126_151501.down,
+    name: '20251126_151501'
   },
 ];
